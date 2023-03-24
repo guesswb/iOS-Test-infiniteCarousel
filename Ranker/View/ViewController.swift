cf70c6e4ee6e604c7332b39cc98b1be8b1aeff81
Kim-Coding <ptibabmgo@gmail.com>
2023-03-25 01:24:43 +0900

    gitignore 추가

---
+++ Ranker/View/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Ranker
+//
+//  Created by 김기훈 on 2023/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
